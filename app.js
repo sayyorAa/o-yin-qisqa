add326ab020c1e0d9dbbb3ded493bfaff9e4263e
sayyorAa <buranovasayyora7@gmail.com>
2026-04-25 17:48:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// Kerakli html tag lariga ulanish
+const first_player_img = document.querySelector(".first__player");
+const second_player_img = document.querySelector(".second__player");
+const result = document.querySelector(".result");
+const btn = document.querySelector(".btn");
+
+result.textContent = 'Siz birinchi o`yinchisiz. Boshlash uchun Start tugmasini bosing!'
+
+// takrorlardan qochish - funksiyaga chiqarish
+function getRandomNumbers(limit = 2) {
+  return Math.round(Math.random() * limit);
+}
+
+// o'yinchilar uchun tasodifiy rasm chiqarish
+function setImageAttribute(random_num, img) {
+  if (random_num == 0) {
+    img.setAttribute("src", "./img/paper.png");
+  } else if (random_num == 1) {
+    img.setAttribute("src", "./img/rock.png");
+  } else {
+    img.setAttribute("src", "./img/scissors.png");
+  }
+}
+
+btn.addEventListener("click", () => {
+  // Tasodifiy sonlarni generatsiya qilish
+  const first_random_number = getRandomNumbers();
+  const second_random_number = getRandomNumbers();
+
+  // Birinchi o'yinchi uchun rasm chiqarish
+  setImageAttribute(first_random_number, first_player_img);
+  // ikkinchi o'yinchi uchun tasodifiy rasm chiqarish
+  setImageAttribute(second_random_number, second_player_img);
+
+  // 0 - paper
+  // 1 - rock
+  // 2 - scissors
+
+  if (
+    (first_random_number == 0 && second_random_number == 1) ||
+    (first_random_number == 1 && second_random_number == 2) ||
+    (first_random_number == 2 && second_random_number == 0)
+  ) {
+    result.textContent = "Siz yutdingiz!";
+    result.style.color = 'green'
+  } else if (first_random_number == second_random_number) {
+    result.textContent = "Durrang!";
+    result.style.color = 'blue'
+  } else {
+    result.textContent = "Siz yutqazdingiz!";
+    result.style.color = 'red'
+  }
+});
